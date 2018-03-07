580f9895fc1974e2b7669bf394d4dfdded93927e
beelarr <bryonl@me.com>
2018-03-07 11:39:10 -0600

    Add container and refactored home and app

---
+++ app/screens/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { Text } from "react-native";
+import { Container } from "../components/Container"
+
+class Home extends Component {
+  render() {
+    return (
+      <Container>
+        <Text>Hello from Home</Text>
+      </Container>
+    );
+  }
+}
+
+export default Home;
