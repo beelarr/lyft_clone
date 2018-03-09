2eed6c26feed4b09bb3a5c8173edf5d2e4b8ebed
beelarr <bryonl@me.com>
2018-03-09 14:33:24 -0600

    Prettier changes

--- app/components/Logo/Logo.js
+++ app/components/Logo/Logo.js
@@ -3,18 +3,17 @@ import { Text, ImageBackground } from "react-native";
 import styles from "./styles";
 import { Actions } from "react-native-router-flux";
 
-
-
-
 export default class Logo extends Component {
-	render() {
-		return (
-					<ImageBackground
-						style={styles.containerImage}
-						source={require('./images/HP-Hero-BG-trim.jpg')}>
-							<Text style={styles.text} onPress={Actions.map}>Login </Text>
-					</ImageBackground>
-		)
-	}
-
-};
+  render() {
+    return (
+      <ImageBackground
+        style={styles.containerImage}
+        source={require("./images/HP-Hero-BG-trim.jpg")}
+      >
+        <Text style={styles.text} onPress={Actions.map}>
+          Login
+        </Text>
+      </ImageBackground>
+    );
+  }
+}
