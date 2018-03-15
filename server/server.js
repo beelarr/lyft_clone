2b32356a6e2cf762e60ce1b4f1b01e091d000ba6
beelarr <bryonl@me.com>
2018-03-15 10:33:14 -0500

    Create a nurse web view and wire up of socket.io

--- server/server.js
+++ server/server.js
@@ -1,30 +1,58 @@
-var express = require('express');
-var path = require('path');
-var bodyParser = require('body-parser');
+// var express = require('express');
+// var path = require('path');
+// var bodyParser = require('body-parser');
+//
+// var index = require('./routes/index');
+// var bookings = require('./routes/bookings');
+//
+// var app = express();
+//
+// var port = 3000;
+//
+// var socket = require('socket.io');
+// var io = socket();
+//
+// app.listen(port, function() {
+//   console.log('Server running on port', port);
+// });
+//
+// //Views
+//
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
+// app.engine('html', require('ejs').renderFile);
+//
+// //Body Parser
+//
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+//
+// //Routes
+//
+// app.use('/', index);
+// app.use('/api', bookings);
+//
+// //Socket
+//
+// io.listen(
+//   app.listen(port, function() {
+//     console.log('Server running on port', port);
+//   })
+// );
 
-var index = require('./routes/index');
-var bookings = require('./routes/bookings');
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
-var app = express();
+server.listen(3000);
 
-var port = 3000;
-
-app.listen(port, function() {
-  console.log('Server running on port', port);
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/views/index.html');
 });
 
-//Views
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.engine('html', require('ejs').renderFile);
-
-//Body Parser
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-//Routes
-
-app.use('/', index);
-app.use('/api', bookings);
+io.on('connection', function(socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function(data) {
+    console.log(data);
+  });
+});
