9a362e49a7e57f3f45dd87a03dbd8f6b168403d1
beelarr <bryonl@me.com>
2018-03-22 13:15:47 -0500

    Refactor services

--- app/components/Services/Services.js
+++ app/components/Services/Services.js
@@ -4,31 +4,27 @@ import { View } from 'react-native';
 import list from '../../data/Services';
 import styles from './styles';
 
-class Services extends React.Component {
-  render() {
-    return (
-      <View>
-        <List containerStyle={styles.listContainer}>
-          {list.map((listItem, i) => (
-            <ListItem
-              roundAvatar
-              avatar={{ uri: listItem.avatar_url }}
-              key={i}
-              title={listItem.name}
-              subtitle={listItem.subtitle}
-            />
-          ))}
-        </List>
-        <Button
-          title="Confirm Service"
-          titleStyle={styles.title}
-          buttonStyle={styles.button}
-          containerStyle={styles.buttonContainer}
-          onPress={this.props.onPress}
+const Services = props => (
+  <View>
+    <List containerStyle={styles.listContainer}>
+      {list.map((listItem, i) => (
+        <ListItem
+          roundAvatar
+          avatar={{ uri: listItem.avatar_url }}
+          key={i}
+          title={listItem.name}
+          subtitle={listItem.subtitle}
         />
-      </View>
-    );
-  }
-}
+      ))}
+    </List>
+    <Button
+      title="Confirm Service"
+      titleStyle={styles.title}
+      buttonStyle={styles.button}
+      containerStyle={styles.buttonContainer}
+      onPress={props.onPress}
+    />
+  </View>
+);
 
 export default Services;
