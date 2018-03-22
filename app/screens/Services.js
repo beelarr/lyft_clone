8e4c9b20b631a532bf1afd463b6e17f520699576
beelarr <bryonl@me.com>
2018-03-22 13:13:12 -0500

    Add alert after confirmtion of booking and route back to map

--- app/screens/Services.js
+++ app/screens/Services.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StatusBar } from 'react-native';
+import { StatusBar, Alert } from 'react-native';
 import { Container } from '../components/Container';
 import { Services } from '../components/Services';
 import { connect } from 'react-redux';
@@ -21,7 +21,7 @@ class ServicesScreen extends Component {
     // For dev - When Services is clicked I am booking that service in the db
     this.props.dispatch(bookServices(user));
 
-    const url = 'http://localhost:3000/api/bookings';
+    const url = 'http://10.0.1.13:3000/api/bookings';
 
     fetch(url, {
       method: 'POST',
@@ -32,14 +32,35 @@ class ServicesScreen extends Component {
     })
       .then(res => res.json())
       .catch(error => console.log('Error', error))
-      .then(response => console.log('Success:', response));
+      .then(response => {
+        console.log('Success:', response);
+        Alert.alert(
+          'Service Booked!',
+          'Click OK to track your nurse.',
+          [
+            {
+              text: 'Cancel',
+              onPress: () => console.log('Cancel Pressed'),
+              style: 'cancel',
+            },
+            {
+              text: 'OK',
+              onPress: () => {
+                Actions.popTo('map');
+                console.log('OK Pressed');
+              },
+            },
+          ],
+          { cancelable: false }
+        );
+      });
   };
 
   render() {
     return (
       <Container>
         <StatusBar hidden={true} barStyle="light-content" />
-        <Services onPress={this.bookHandler && Actions.map} />
+        <Services onPress={this.bookHandler} />
       </Container>
     );
   }
